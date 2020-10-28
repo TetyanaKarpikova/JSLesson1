5b28bb23fe849a2254051424a2b040b7074d1170
Tetyana <tetyanajkn@gmail.com>
2020-10-28 21:24:27 +0200

    basic task

---
+++ ls1.js
@@ -0,0 +1,197 @@
+//  // tasks 1
+// let str_hello = "hello";
+// let str_owu = "owu";
+// let str_com = "com";
+// let str_ua = "ua";
+// let num_first = 1;
+// let num_ten = 10;
+// let num_minus = -999;
+// let num_start = 123;
+// let num_pi = 3.14;
+// let num2 = 2.7;
+// let num_steen = 16;
+// let isTrue = true;
+// let isFalse = false;
+
+// console.log(str_hello, str_owu, str_com, str_ua);
+// alert(str_hello + '  ' + str_owu)
+// alert(str_com);
+// alert(str_ua);
+// document.write(str_hello +  '  ' + str_owu + '  ' +  str_com);
+// document.write(str_ua);
+// document.write('<br>');
+// console.log(num_first, num_ten, num_minus, num_start, num_pi, num2, num_steen);
+// alert(num_first + '  ' + num_ten + '  ' +num_minus);
+// alert(num_start + '  ' + num_pi + '  ' + num2 + '  ' + num_steen);
+// document.write(num_first + '  ' + num_ten + '  ' +num_minus);
+// document.write(num_start + '  ' + num_pi + '  ' + num2 + '  ' + num_steen);
+// document.write('<br>');
+// console.log(isTrue);
+// console.log(isFalse);
+// alert(isTrue);
+// alert(isFalse);
+// document.write(isTrue);
+// document.write('<br>');
+// document.write(isFalse);
+
+
+
+//  // task 2
+// let str_hello = "write";
+// let str_owu = "new";
+// let str_com = "interesting";
+// let str_ua = "story";
+// let num_first = 0.001;
+// let num_ten = 1000;
+// let num_minus = -150.25;
+// let num_start = 11235;
+// let num_pi = 14.35879;
+// let num2 = 22002;
+// let num_steen = -1600;
+// let isTrue = true;
+// let isFalse = false;
+
+// console.log(str_hello, str_owu, str_com, str_ua);
+// alert(str_hello + '  ' + str_owu)
+// alert(str_com);
+// alert(str_ua);
+// document.write(str_hello +  '  ' + str_owu + '  ' +  str_com);
+// document.write(str_ua);
+// document.write('<br>');
+// console.log(num_first, num_ten, num_minus, num_start, num_pi, num2, num_steen);
+// alert(num_first + '  ' + num_ten + '  ' +num_minus);
+// alert(num_start + '  ' + num_pi + '  ' + num2 + '  ' + num_steen);
+// document.write(num_first + '  ' + num_ten + '  ' +num_minus);
+// document.write(num_start + '  ' + num_pi + '  ' + num2 + '  ' + num_steen);
+// document.write('<br>');
+// console.log(isTrue);
+// console.log(isFalse);
+// alert(isTrue);
+// alert(isFalse);
+// document.write(isTrue);
+// document.write('<br>');
+// document.write(isFalse);
+
+
+// // task3
+// const num1 = 123;
+// const num2 = 9.806;
+// const num3 = -1024;
+// const str1 = 'Start';
+// const str2 = 'luck';
+// const str3 = 'constant';
+
+// console.log(num1);
+// console.log(num2);
+// console.log(num3);
+// console.log(str1);
+// console.log(str2);
+// console.log(str3);
+// alert(num1);
+// alert(num2);
+// alert(num3);
+// alert(str1);
+// alert(str2);
+// alert(str3);
+// document.write('<br>');
+// document.write(num1 + '  ' + num2 + '  ' + num3);
+// document.write('<br>');
+// document.write(str1 + '  ' + str2 + '  ' + str3);
+
+
+//  //task4
+// let last_name = prompt('What is you last name?');
+// let name = prompt('What is you name?');
+// let patronymic = prompt('What is you patronymic?');
+
+// document.write('<br>');
+// console.log(last_name);
+// console.log(name);
+// console.log(patronymic);
+// alert(last_name);
+// alert(name);
+// alert(patronymic);
+// document.write(last_name);
+// document.write(name);
+// document.write(patronymic);
+
+
+// //task5
+// let last_name = prompt('What is you last name?');
+// let name = prompt('What is you name?');
+// let patronymic = prompt('What is you patronymic?');
+// let person = last_name + ' ' + name + ' ' + patronymic;
+
+// // task7
+// let num1 = +prompt('Введіть перше число:');
+// let num2 = +prompt('Введіть друге число');
+// let num3 = +prompt('Введіть третє число');
+// console.log(num1);
+// console.log(num2);
+// console.log(num3);
+
+// // task8
+// let num_Int1 = parseInt(prompt('Введіть перше число Integer:'));
+// let num_Int2 = parseInt(prompt('Введіть друге число Integer:'));
+// let num_Int3 = parseInt(prompt('Введіть третє число Integer:'));
+// let num_Int4 = parseInt(prompt('Введіть четвертє число Integer:'));
+// let result = num_Int1 + num_Int2 + num_Int3 + num_Int4;
+// console.log(result);
+
+// // task9
+// let num_Float1 = parseFloat(prompt('Введіть перше число Float:'));
+// let num_Float2 = parseFloat(prompt('Введіть друге число Float:'));
+// let num_FLoat3 = parseFloat(prompt('Введіть третє число Float:'));
+// let result = num_Float1 + num_Float2 + num_FLoat3;
+// console.log(result);
+
+
+// // task10
+// let num_Fl1 = parseFloat(prompt('Введіть перше число Float:'));
+// let num_Fl2 = parseFloat(prompt('Введіть друге число Float:'));
+// let num_Fl3 = parseFloat(prompt('Введіть третє число Float:'));
+// let result_Fl = Math.round(num_Fl1) + Math.round(num_Fl2) + Math.round(num_Fl3);
+// console.log(result_Fl);
+
+// // task11
+// let num_I1 = parseInt(prompt('Введіть перше число Integer:'));
+// let num_I2 = parseInt(prompt('Введіть друге число Integer:'));
+// let result_pow = Math.pow(num_I1, num_I2);
+// console.log(result_pow);
+
+// // task12
+// let a = 100;
+// let b = '100';
+// let c = true;
+// let d = undefined;
+// console.log(typeof(a));
+// console.log(typeof(b));
+// console.log(typeof(c));
+// console.log(typeof(d));
+
+
+// //task13
+// let isMin6 = 5 < 6;
+// let isMax5 = 5 > 6;
+// let isNot5 = 5 == 6;
+// let isEq5 = 5 >= 6;
+// let isEq10 = 10 == 10;
+// let isMax10 = 10 >= 10;
+// let isMit10 = 10 < 10;
+// let isNot10 = 10 != 10;
+// let isA = 10 > 10;
+// let isStr = 123 === '123';
+// let isInt = 123 == '123';
+
+// console.log(isMin6);
+// console.log(isMax5);
+// console.log(isNot5);
+// console.log(isEq5);
+// console.log(isEq10);
+// console.log(isMax10);
+// console.log(isMit10);
+// console.log(isNot10);
+// console.log(isA);
+// console.log(isStr);
+// console.log(isInt);
+
