8c30e3b1d9c9e644217edd4429a7ff28c85fb25f
Tetyana <tetyanajkn@gmail.com>
2020-10-30 18:55:46 +0200

    added class task

---
+++ practice.js
@@ -0,0 +1,121 @@
+// // CLASS
+// // task1
+// let str = "Привет";
+// let num = 123;
+// let flag = true;
+// let txt = "true";
+
+// console.log(typeof(str));
+// console.log(typeof(num));
+// console.log(typeof(flag));
+// console.log(typeof(txt));
+
+
+// // task2
+// let a1 = 5 + 3;
+// let a2 = 5 - 3;
+// let a3 = 5 * 3;
+// let a4 = 5 / 3;
+// let a5 = 5 % 3;
+
+// console.log(a1);
+// console.log(a2);
+// console.log(a3);
+// console.log(a4);
+// console.log(a5);
+
+
+// // task3
+// let a6 = 5 % 3;
+// let a7 = 3 % 5;
+// let a8 = 5 + '3';
+// let a9 = '5' - 3;
+// let a10 = 75 + 'кг';
+
+// console.log(a6);
+// console.log(a7);
+// console.log(a8);
+// console.log(a9);
+// console.log(a10);
+
+
+// // task4
+// let height = 23;
+// let width = 10;
+// let s = height * width;
+
+// console.log(s);
+
+
+// // task5
+// let heightC = 10;
+// let dc = 4;
+// let v = ((Math.PI * Math.pow(dc, 2)) /4 ) * heightC;
+//  console.log(v);
+
+
+// // task6
+// let n = 3;
+// let m = 4;
+// // let k = Math.pow((Math.pow(n, 2) + Math.pow(m, 2)), 0.5);
+// let k = (n**2 + m**2)**0.5;
+// console.log(k);
+
+
+// // task7
+// let str = "Hello world";
+// document.write(str);
+// alert(str);
+// console.log(str);
+
+
+// // task8
+// let name = 'Tetyana Karpikova';
+// let age = 36;
+// let hobby = 'cooking';
+// alert(name + '\n' + age + '\n' + hobby);
+
+
+// // task9
+// let str1 = 'Кто ';
+// let str2 = 'ты ';
+// let str3 = 'такой?';
+// let concatenation = str1 + str2 + str3;
+// document.write(concatenation);
+
+
+// // task 10
+// let str = "20";
+// let a = 5;
+// document.write(str + a + "<br/>");    //  205
+// document.write(str - a + "<br/>");    // 15
+// document.write(str * "2" + "<br/>");  // 40
+// document.write(str / 2 + "<br/>");    // 10
+
+
+// // task 11
+// parseInt("3.14")    // 3
+// parseInt("-7.875")  // -7
+// parseInt("435")     //435
+// parseInt("Вася")    // Nan  
+
+
+// // task 12
+// let str = prompt('Enter something', 'ho-ho');
+// console.log(str);
+
+
+// // task 13
+// let num1 = parseFloat(prompt("Введіть перше число:"));
+// let num2 = parseFloat(prompt("Введіть друге число:"));
+
+// alert(num1 + num2);
+
+
+// // task 14
+// let name = prompt("Введіть ваше ім'");
+// let last_name = prompt("Введіть ваше прізвище:");
+// let age = prompt("Введіть ваш вік:");
+// alert("Доброго вечера "+ name + " " + last_name + ", мои поздравления что вам " + age);
+
+
